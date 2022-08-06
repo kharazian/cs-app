ade57ae49906cbf831cf77c4f6d55f56e8697a70
Kharazian <AmirKharazian@gmail.com>
2022-08-06 02:12:17 -0700

    init theme

---
+++ src/App.react.js
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  ForgotPasswordPage,
+  LoginPage,
+  RegisterPage,
+  Error400,
+  Error401,
+  Error403,
+  Error404,
+  Error500,
+  Error503,
+  Empty,
+  Email,
+  ProfilePage,
+} from "./pages";
+
+import HomePage from "./HomePage.react";
+
+import "tabler-react/dist/Tabler.RTL.css";
+
+type Props = {||};
+
+function App(props: Props): React.Node {
+  return (
+    <React.StrictMode>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/400" component={Error400} />
+          <Route exact path="/401" component={Error401} />
+          <Route exact path="/403" component={Error403} />
+          <Route exact path="/404" component={Error404} />
+          <Route exact path="/500" component={Error500} />
+          <Route exact path="/503" component={Error503} />
+          <Route exact path="/email" component={Email} />
+          <Route exact path="/empty-page" component={Empty} />
+          <Route exact path="/forgot-password" component={ForgotPasswordPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/profile" component={ProfilePage} />
+          <Route exact path="/register" component={RegisterPage} />
+          <Route component={Error404} />
+        </Switch>
+      </Router>
+    </React.StrictMode>
+  );
+}
+
+export default App;
